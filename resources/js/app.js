5bafdeaad0510b525c4bc9cd027ed17decfcb1a3
Irving Dinh <irving.dinh@gmail.com>
2022-08-31 14:36:51 +0700

    Added something, maybe bug... 🐛 🐞 🪲

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+
+import {InertiaProgress} from '@inertiajs/progress'
+import {createApp, h} from 'vue'
+import {createInertiaApp} from '@inertiajs/inertia-vue3'
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers'
+
+createInertiaApp({
+  resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')),
+  setup({el, App, props, plugin}) {
+    createApp({render: () => h(App, props)})
+      .use(plugin)
+      .mount(el)
+
+    InertiaProgress.init()
+  },
+})
